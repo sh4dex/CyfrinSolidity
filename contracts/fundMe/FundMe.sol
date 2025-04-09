2c8d32279411400a98bb4bb47180e88b451f539f
sh4dex <sthx09567541@gmail.com>
2025-04-09 20:58:25 +0000

    constructor

--- contracts/fundMe/FundMe.sol
+++ contracts/fundMe/FundMe.sol
@@ -17,19 +17,37 @@ contract FundMe{
     address[] public funders;
     mapping(address funder=> uint256 amountFunded) public addressToAmountFunded;
 
+    address public owner;
+
+    constructor (){
+        owner = msg.sender;
+    }
+
     //The payable keyword indicate that the function can revice ETH.
     function fund() public payable{
         require(msg.value.getConversionRate() >= minimunUsdAmount, "Not enough ETH avaible to be sent.");
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] += msg.value;
     }
-
     
     function withdra() public {
 
         for(uint256 funderIndex =0; funderIndex<funders.length ;funderIndex++){
             addressToAmountFunded[funders[funderIndex]] = 0;
         }
+        //new blank
+        funders = new address[](0);
+
+        //- transfer
+        //payable(msg.sender).transfer(address(this).balance);
+        
+        //- send (no auto revert)
+        //bool success = payable (msg.sender).send(address(this).balance);
+        //require(success, "Send has failed.");
+        
+        //-call
+        (bool callSuccess, /*bytes memory dataReturned*/) = payable(msg.sender).call{value: address(this).balance}(""); 
+        require(callSuccess, "Call has failed.");
     }
     
 }
